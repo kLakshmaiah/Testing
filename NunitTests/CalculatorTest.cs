f2e0fe792a7fee21db32020cc8ef7f2515d08457
KolliLakshmaiah1219 <Kolli.Lakshmaiah@sightspeak.ai>
2024-07-29 12:11:31 +0530

    Add project files.

---
+++ NunitTests/CalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using UnitTestPractise;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        [Test]
+        public void AddTwoNumbers_InptutAsTwoInteger()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.AddTwoNumbers(10, 20), Is.EqualTo(30));
+            Assert.That(calculator.AddTwoNumbers(20, 10), Is.EqualTo(10));
+        }
+    }
+}
---
+++ UnitTestPractise/Calculator.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTestPractise
+{
+    public class Calculator
+    {
+        public int AddTwoNumbers(int a ,int b)
+        {
+            if(a < b)
+                return a+b;
+            return a-b;
+        }
+    }
+}
