e327bbcceddba97693eb6a0e345c2354e8e16ea6
KolliLakshmaiah1219 <Kolli.Lakshmaiah@sightspeak.ai>
2024-07-29 17:30:35 +0530

    remvoing the coverlete packages and adding the testing class in same project

---
+++ NunitTests/CodeClass/UserDetails.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitTests.CodeClass
+{
+    public class UserDetails
+    {
+        public bool Login(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName))
+            {
+                throw new ArgumentException("user Name is Empty");
+            }
+            else if (string.IsNullOrEmpty(password))
+            {
+                throw new ArgumentException("Password is Empty");
+            }
+            else
+            {
+                if (userName.Equals("Laxman", StringComparison.CurrentCultureIgnoreCase) && userName.Equals("admin@1234", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ NunitTests/UserDetailsNUnitTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using NunitTests.CodeClass;
+
+namespace NunitTests
+{
+    [TestFixture]
+    public class UserDetailsNUnitTest
+    {
+       private UserDetails? userDetails {  get; set; } 
+        [SetUp]
+        public void SetUp()
+        {
+             userDetails= new();
+        }
+        [Test]
+        public void UserDetails_UserName_IsNull_GetException()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                userDetails?.Login(string.Empty, string.Empty);
+            });
+            Assert.Throws<ArgumentException>(() =>
+            {
+                userDetails?.Login("Laxman", string.Empty);
+            });
+        }
+    }
+}
